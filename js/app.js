d795c4e069975934a52c0cf6917697669016bd55
Isabella Xiong <IXiong@ucvts.tec.nj.us>
2019-12-16 10:42:35 -0500

    added input

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let emptyArray = [];
+window.onload = function() {
+  document.getElementById("events")
+  let h =  document.getElementById("events")
+}
+letters.push(h);
+
+get elements, get input, store it into the array, take the array
