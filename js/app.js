b4f76e688e3a42c410cce5600e43809753741673
ixiong122 <isabellax122@gmail.com>
2019-12-26 23:47:46 -0500

    three out of four functions

--- js/app.js
+++ js/app.js
@@ -1,8 +1,42 @@
-let emptyArray = [];
-window.onload = function() {
-  document.getElementById("events")
-  let h =  document.getElementById("events")
-}
-letters.push(h);
+var ul = document.getElementById('list')
+var li;
+var addButton = document.getElementById('add')
+addButton.addEventListener("click",addItem)
 
-get elements, get input, store it into the array, take the array
+function addItem(){
+  var input = document.getElementById('input')
+  var item = input.value;
+  var textNode = document.createTextNode(item)
+    if (item == "") {
+      alert("Please enter your task.");
+      return false
+    } else {
+      li = document.createElement('li')
+      let checkbox = document.createElement('input')
+      checkbox.type = 'checkbox'
+      checkbox.setAttribute('id','check')
+
+      let label = document.createElement('label')
+
+      ul.appendChild(label)
+      li.appendChild(checkbox)
+      label.appendChild(textNode)
+      li.appendChild(label)
+      ul.insertBefore(li, ul.childNodes[0])
+      setTimeout(()=>{
+       li.className = 'visual'},5)
+      input.value = " "
+    }
+}
+var removeButton = document.getElementById('remove')
+removeButton.addEventListener("click",removeItem)
+
+function removeItem(){
+  li=ul.children
+  for (let index = 0; index < li.length; index++) {
+    const element = li[index];
+    while(li[index] && li[index].children[0].checked){
+     ul.removeChild(li[index])
+        }
+    }
+}
