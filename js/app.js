b0f4c71a6b556541476a186adb7ca9a967b32eec
ixiong122 <isabellax122@gmail.com>
2020-01-02 10:49:33 -0500

    s

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ function addItem(){
   var input = document.getElementById('input')
   var item = input.value;
   var textNode = document.createTextNode(item)
+  do {
     if (item == "") {
       alert("Please enter your task.");
       return false
@@ -15,18 +16,19 @@ function addItem(){
       let checkbox = document.createElement('input')
       checkbox.type = 'checkbox'
       checkbox.setAttribute('id','check')
-
+      var text = document.createIMG();
       let label = document.createElement('label')
-
       ul.appendChild(label)
       li.appendChild(checkbox)
       label.appendChild(textNode)
       li.appendChild(label)
+      label.appendChild(text)
       ul.insertBefore(li, ul.childNodes[0])
       setTimeout(()=>{
        li.className = 'visual'},5)
       input.value = " "
     }
+  } while(item == "");
 }
 var removeButton = document.getElementById('remove')
 removeButton.addEventListener("click",removeItem)
@@ -40,3 +42,12 @@ function removeItem(){
         }
     }
 }
+  function prioritizeItem() {
+    li = ul.children
+    for (let index = 0; index < li.length; index++) {
+      const element2 = li[index];
+      while (li[index] && li[index].children[0].checked) {
+
+      }
+    }
+  }
